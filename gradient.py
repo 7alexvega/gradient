6f9525e0864ec2b8e90d963f3f220a929656476c
7alexvega <7alexvega@gmail.com>
2022-01-24 00:04:32 -0800

    added caching of wallpapers

--- gradient.py
+++ gradient.py
@@ -1,4 +1,6 @@
 import atexit
+import json
+import os
 import time
 
 from config import config
@@ -8,45 +10,64 @@ from spotify import Spotify
 from windows import Windows
 
 
-def main():
-    spotify = Spotify()
-    windows = Windows()
-    image = Image()
-    hue = Hue()
+class Gradient:
 
-    atexit.register(windows.revert_wallpaper)
+    def __init__(self):
+        self.cache = self.get_cache()
 
-    previous_song = None
-
-    while True:
-        current_song = windows.get_current_song()
-
-        if current_song:
-
-            song_changed = current_song != previous_song
-            if song_changed:
-                previous_song = current_song
-
-                album_art_url = spotify.get_current_album_art_url()
-                album_art = spotify.get_album_art_image(album_art_url)
-                name = album_art_url.rsplit('/')[-1]
-
-                wallpaper_path = image.create_wallpaper(album_art, name)
-                windows.set_wallpaper(wallpaper_path)
-
-                hue_lights = config['hue']['lights']
-
-                dominant_colors = image.get_dominant_colors(wallpaper_path, len(hue_lights))
-                for i in range(len(dominant_colors)):
-                    light = hue_lights[i]
-                    color = dominant_colors[i]
-                    hue.update_light_color(light, color)
-
+    def get_cache(self):
+        if os.path.exists('db.json'):
+            with open('db.json', 'r') as file:
+                cache = json.load(file)
         else:
-            windows.revert_wallpaper()
+            cache = dict()
+        return cache
 
-        time.sleep(config['gradient']['refresh_delay_s'])
+    def start(self):
+        spotify = Spotify()
+        windows = Windows()
+        image = Image()
+        hue = Hue()
+
+        atexit.register(windows.revert_wallpaper)
+        atexit.register(lambda: json.dump(self.cache, open('db.json', 'w')))
+
+        previous_song = None
+
+        while True:
+            current_song = windows.get_current_song()
+
+            if current_song:
+
+                song_changed = current_song != previous_song
+                if song_changed:
+                    previous_song = current_song
+
+                    album_art_url = spotify.get_current_album_art_url()
+                    album_key = album_art_url.rsplit('/')[-1]
+
+                    if album_key in self.cache and os.path.exists(self.cache[album_key]):
+                        wallpaper_path = self.cache[album_key]
+                    else:
+                        album_art = spotify.get_album_art_image(album_art_url)
+                        wallpaper_path = image.create_wallpaper(album_art, album_key)
+                        self.cache[album_key] = wallpaper_path
+
+                    hue_lights = config['hue']['lights']
+
+                    dominant_colors = image.get_dominant_colors(wallpaper_path, len(hue_lights))
+                    for i in range(len(dominant_colors)):
+                        light = hue_lights[i]
+                        color = dominant_colors[i]
+                        hue.update_light_color(light, color)
+                    windows.set_wallpaper(wallpaper_path)
+
+            else:
+                windows.revert_wallpaper()
+
+            time.sleep(config['gradient']['refresh_delay_s'])
 
 
 if __name__ == '__main__':
-    main()
+    gradient = Gradient()
+    gradient.start()
